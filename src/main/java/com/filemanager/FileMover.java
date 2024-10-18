e17760b3d1fb6ed8752109bbf6ee363f6e4c4ca5
Thobani Mabaso <tmabaso023@student.wethinkcode.co.za>
2024-10-18 19:11:15 +0200

    fixed the issue with moving files

--- src/main/java/com/filemanager/FileMover.java
+++ src/main/java/com/filemanager/FileMover.java
@@ -1,11 +1,25 @@
 package com.filemanager;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileMover {
     public static boolean moveFile(String sourcePath, String destinationPath) {
-        File source = new File(sourcePath);
-        File dest = new File(destinationPath);
-        return source.renameTo(dest);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        processBuilder.command("mv", sourcePath, destinationPath);
+
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return true;
+            } else {
+                System.out.println("Failed to move file. Exit code: " + exitCode);
+            }
+        } catch (InterruptedException | IOException e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+        return false;
     }
 }
